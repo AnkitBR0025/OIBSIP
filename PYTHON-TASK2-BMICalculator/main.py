bc7996111fe8f6688e0f85a8ca81fa60514ec726
Ankit <ankitpandey12348409@gmail.com>
2026-09-15 18:40:27 +0530

    Update Files

--- PYTHON-TASK2-BMICalculator/main.py
+++ PYTHON-TASK2-BMICalculator/main.py
@@ -2,38 +2,48 @@
 # Track:- Python Programming
 # Task Title:- BMI Calculator
 
-
 print("--- CHECK YOUR BMI (Body Mass Index) ---")
 
-try:
-    weight = float(input("Enter your weight in kg: "))
-    height = float(input("Enter your height in feet: "))
+while True:
+    try:
+        weight = float(input("\nEnter your weight in kg: "))
+        height = float(input("Enter your height in feet: "))
 
-    # Check for negative or zero values
-    if weight <= 0 or height <= 0:
-        print("Error: Weight and height must be greater than 0.")
-    else:
-        # Convert height from feet to meters
-        height_meter = height * 0.3048
+       
+        if weight <= 0 or height <= 0:
+            print("Error: Weight and height must be greater than 0.")
 
-        # Calculate BMI
-        bmi = weight / (height_meter ** 2)
-
-        # Determine BMI category
-        if bmi < 18.5:
-            category = "Underweight"
-        elif bmi < 25:
-            category = "Normal 👍"
-        elif bmi < 30:
-            category = "Overweight"
         else:
-            category = "Obese 🐘"
+            
+            height_meter = height * 0.3048
 
-        print(f"\nYour weight is {weight} kg and height is {round(height_meter, 2)} m.")
-        print("\nResult:-")
-        print(f"Your BMI is: {round(bmi, 2)}")
-        print(f"Category: {category}")
+           
+            bmi = weight / (height_meter ** 2)
 
-except ValueError:
-    print("Error: Please enter numeric values only.")
+            
+            if bmi < 18.5:
+                category = "Underweight"
+            elif bmi < 25:
+                category = "Normal 👍"
+            elif bmi < 30:
+                category = "Overweight"
+            else:
+                category = "Obese 🐘"
+
+            print(f"\nYour weight is {weight} kg and height is {round(height_meter, 2)} m.")
+
+            print("\nResult:-")
+            print(f"Your BMI is: {round(bmi, 2)}")
+            print(f"Category: {category}")
+
+    except ValueError:
+        print("Error: Please enter numeric values only.")
+
+
+    choice = input("\nDo you want to check another BMI? (y/n): ").lower()
+
+    if choice == "n":
+        print("\nThank you for using the BMI Calculator!")
+        break
+    
 
